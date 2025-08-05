a270f89d5fef14aa67e963387003785ad6c98874
Simran Kumari <149223456+simrankumari-dev@users.noreply.github.com>
2025-08-06 02:02:37 +0530

    Time: 2 ms (97.88%), Space: 43.4 MB (66.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //Checking if the length of string same or not , If not same then definately it will be not a valid anagram
         if(s.length()!=t.length()){
             return false;
-
         }
-        //We do not have a sorting method in string to sort the string so we have to convert it into charArray
-        char[] sArr=s.toCharArray();
-        char[] tArr=t.toCharArray();
-
-        //sort both arrays
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
-
-        //Commpare sorted arrays
-        return Arrays.equals(sArr,tArr);
-
-     
-
-
+        int count[]=new int[26];//we assume only lower case letters
+        
+        for(char ch:s.toCharArray()){
+            count[ch-'a']++;
+            
+        }
+        for(char ch:t.toCharArray()){
+            count[ch-'a']--;
+        }
+        for(int val:count){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
         
     }
 }
