47b32f395bfe424d4cad4f3559c546cd83155e63
Simran Kumari <149223456+simrankumari-dev@users.noreply.github.com>
2025-08-06 02:08:01 +0530

    Update 0242-valid-anagram.java
    Commenting the  valid anagram  problem

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,14 +5,14 @@ class Solution {
         }
         int count[]=new int[26];//we assume only lower case letters
         
-        for(char ch:s.toCharArray()){
+        for(char ch:s.toCharArray()){  //Loop of incrementing the value of count as its correct place
             count[ch-'a']++;
             
         }
-        for(char ch:t.toCharArray()){
+        for(char ch:t.toCharArray()){ // Loop of decrementing the value of count when we get the sepcifiecd character in string 2 which is already in string1 
             count[ch-'a']--;
         }
-        for(int val:count){
+        for(int val:count){  // Checking if count has number other than 1 if yes then return false as this is not the anagrams case 
             if(val!=0){
                 return false;
             }
