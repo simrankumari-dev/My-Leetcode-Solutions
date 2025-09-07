97d511d68839da44491a9ab4322d24e7d5af90ef
Simran Kumari <149223456+simrankumari-dev@users.noreply.github.com>
2025-09-07 23:31:49 +0530

    Time: 12 ms (19.7%), Space: 45.1 MB (9.62%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Using HashSet to track seen numbers
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    // Create unique keys for row, col, and box
+                    String rowKey = num + " in row " + i;
+                    String colKey = num + " in col " + j;
+                    String boxKey = num + " in box " + (i / 3) + "-" + (j / 3);
+
+                    // If already exists, invalid board
+                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
