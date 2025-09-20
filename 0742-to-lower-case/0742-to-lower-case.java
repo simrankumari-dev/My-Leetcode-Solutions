21c0d11e2f269bc40506e038b07468dcc408eb59
Simran Kumari <149223456+simrankumari-dev@users.noreply.github.com>
2025-09-20 23:10:20 +0530

    Time: 0 ms (100%), Space: 42.1 MB (10.23%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,14 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder result = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            // If c is uppercase (A-Z), convert to lowercase
-            if (c >= 'A' && c <= 'Z') {
-                result.append((char)(c + 32)); // ASCII conversion
-            } else {
-                result.append(c); // keep same
-            }
-        }
-        return result.toString();
+        return s.toLowerCase();
     }
 }
